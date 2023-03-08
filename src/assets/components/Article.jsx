af84f69ea8088fdf58bca3015f6ca4f07a668ec3
C109156201 <C109156201@nkust.edu.tw>
2023-03-08 16:34:45 +0800

    To React

---
+++ src/assets/components/Article.jsx
@@ -0,0 +1,81 @@
+export default function Article(params) {
+    return (
+        <div>
+            <article className="image__layout py-3 py-sm-5">
+                <div className="container">
+                <h1 className="text-center">IMAGES</h1>
+                <hr className="divider--dark"/>
+                    <div className="row">
+                        <div className="image mt-4 col-sm-6 col-lg-3">
+                        <a href="" className="image__link">
+                            <img src="images/img1.jpg" alt="" className="image__style"/>
+                        </a>
+                        <p className="image_title">Place 1</p>
+                        </div>
+                        <div className="image mt-4 col-sm-6 col-lg-3">
+                        <a href="" className="image__link">
+                            <img src="images/img2.jpg" alt="" className="image__style"/>
+                        </a>
+                        <p className="image_title">Place 2</p>
+                        </div>
+                        <div className="image mt-4 col-sm-6 col-lg-3">
+                        <a href="" className="image__link">
+                            <img src="images/img3.jpg" alt="" className="image__style"/>
+                        </a>
+                        <p className="image_title">Place 3</p>
+                        </div>
+                        <div className="image mt-4 col-sm-6 col-lg-3">
+                        <a href="" className="image__link">
+                            <img src="images/img4.jpg" alt="" className="image__style"/>
+                        </a>
+                        <p className="image_title">Place 4</p>
+                        </div>
+                        <div className="image mt-4 col-sm-6 col-lg-3">
+                        <a href="" className="image__link">
+                            <img src="images/img5.jpg" alt="" className="image__style"/>
+                        </a>
+                        <p className="image_title">Place 5</p>
+                        </div>
+                        <div className="image mt-4 col-sm-6 col-lg-3">
+                        <a href="" className="image__link">
+                            <img src="images/img6.jpg" alt="" className="image__style"/>
+                        </a>
+                        <p className="image_title">Place 6</p>
+                        </div>                
+                        <div className="image mt-4 col-sm-6 col-lg-3">
+                        <a href="" className="image__link">
+                            <img src="images/img7.jpg" alt="" className="image__style"/>
+                        </a>
+                        <p className="image_title">Place 7</p>
+                        </div>
+                        <div className="image mt-4 col-sm-6 col-lg-3">
+                        <a href="" className="image__link">
+                            <img src="images/img8.jpg" alt="" className="image__style"/>
+                        </a>
+                        <p className="image_title">Place 8</p>
+                        </div>   
+                    </div>
+                </div>
+            </article>
+
+            <article className="description__layout py-4 py-sm-5">
+                <div className="container d-flex flex-column align-items-center">
+                <h1>DESCRIPTIONS</h1>
+                <hr className="divider--light"/>
+                <p className="text-justify description__content py-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laborum
+                    corrupti ut iure aliquam. Quod, perferendis. Libero tempore iste, animi sint numquam illum quasi laboriosam
+                    unde pariatur quidem! Suscipit consequatur nihil dolor impedit temporibus ad cum, voluptatum odit ratione
+                    ullam
+                    eligendi amet beatae aliquid eaque nesciunt optio nobis ducimus itaque. Voluptatibus aspernatur assumenda
+                    quod
+                    quo error consequuntur, vero iusto reprehenderit repellat maiores, sapiente dolores voluptates minima. Alias
+                    dolore assumenda fuga quae repellat voluptatum quo, saepe exercitationem perspiciatis reiciendis modi
+                    tempora.
+                    Recusandae explicabo iusto corporis nam numquam dignissimos, necessitatibus, iure non culpa repellat
+                    repudiandae temporibus! Saepe excepturi tempore iusto eos sit!</p>
+                </div>
+            </article>
+        </div>
+    );
+}
+    
---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from './assets/components/Header';
+import Footer from './assets/components/Footer';
+import Article from './assets/components/Article';
+
+function Home(params) {
+    return(
+        <div>
+            <Header
+                Home = "Home"
+                Slogan = "Slogan"
+            />
+            <Article></Article>
+            <Footer></Footer>
+        </div>
+    );
+}
+export default Home;

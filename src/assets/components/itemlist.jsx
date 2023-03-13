c9ec5e441d051110f4016518bf9bc6a7785182b6
ho-chi <s110919047@stu.ntue.edu.tw>
2023-03-13 11:32:17 +0800

    modify css

--- src/assets/components/itemlist.jsx
+++ src/assets/components/itemlist.jsx
@@ -5,13 +5,14 @@ import { Row,Col } from "antd";
 export default function itemlist() {
     
     return (
-        <Row gutter={[24,24]} >
+        <Row gutter={[32,32]} >
         {product.map(products => (
             <Col
                 key={products.id}
                 sm={{ span: 12 }}
-                lg={{ span: 6 }}
-                
+                lg={{ span: 8 }}
+                xl={{ span: 6 }}
+                xxl={{ span: 4 }}
             >    
                 <Item
                     products={products}

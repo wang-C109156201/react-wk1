1be6fcdd94262108711c918107402ad05e288df5
ho-chi <s110919047@stu.ntue.edu.tw>
2023-03-13 11:23:09 +0800

    classname

--- src/assets/components/item.jsx
+++ src/assets/components/item.jsx
@@ -1,6 +1,6 @@
 export default function item({products}) {
     return(
-        <section className="image mt-4 col-sm-6 col-lg-3" key={products.id}>
+        <section className="image" key={products.id}>
                 <div >
                     <a href="" className="image__link">
                         <img  className="image__style"
